b4be8a6488334ae71c7b1951d39dfe602de3ee1b
Hooder <hoodernew@gmail.com>
2025-01-20 11:33:46 +1300

    Update
    using sonic as a base
    (could be saved for future ideas)

--- BetaSFMLTesting/AlphaV1/AlphaV1.cpp
+++ BetaSFMLTesting/AlphaV1/AlphaV1.cpp
@@ -1,48 +1,57 @@
 #include <iostream>
+// Required SFML Headers
 #include "SFML/Graphics.hpp"
 #include "SFML/Window.hpp"
 
-class playerData 
+class sonicData 
 {
-public:
-    
-    void playerSprite() 
-    {
-        sf::Texture lightGlass;
-        if (!lightGlass.loadFromFile("C:\\Users\\The Hooder\\Desktop\\ProjectOldVillage-SFML-Horror-Game-\\Assets\\TEMP\\glass_light_blue.png")) 
-        {
+  public:
 
-        }
-    }
+      void sonicStand() 
+      {
+          sf::Texture sonTex1; // (Sonic Texture 01)
+          if (!sonTex1.loadFromFile("C:\\Users\\Hooder\\Desktop\\ProjectOldTown\\ProjectOldVillage-SFML-Horror-Game-\\Assets\\TEMP\\Sonic\\SonicIdle.png"));
+      }
 };
 
 
 int BaseWindow() // always best to use a function that can return a value!
 {
     sf::RenderWindow window(sf::VideoMode(960, 540), "SFML window");
+    // This will create a new window and set it's width and height
 
     // program will stay at 60 frames per second 
     window.setFramerateLimit(60);
 
     // change the title of the window (ignores last change in "Render Window" above)
-    window.setTitle("Version: BETA");
+    window.setTitle("Frontiers Beta0.1");
+    //
 
+    //
     // run the program as long as the window is open
     while (window.isOpen()) 
     {
-        // creates an Event called "eventTrigger" 
-        sf::Event eventTrigger;
+        sf::Event closeApp; // new event related for closing the window
 
-        playerData myobj;
-        myobj.playerSprite();
-
-        while (window.pollEvent(eventTrigger))
+        while (window.pollEvent(closeApp)) // while during the event (close application)
         {
-            
-            if (eventTrigger.type == sf::Event::Closed)
+            switch (closeApp.type) 
+            {
+                case sf::Event::Closed: // looks if window is closed
+                    window.close(); //application closes
 
-                window.close();
+                    break;
+            }
         }
+
+
+        //Render
+        window.clear(sf::Color::Cyan); // Sets a new const color via the frame
+
+
+        //draw app here
+
+        window.display();
     }
 
     return 0;
